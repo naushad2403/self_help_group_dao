2e52ed111f4f6e17472d02668187136c9b517167
MehakB7 <b7mehak@gmail.com>
2023-07-15 20:10:16 +0530

    Updated balance events

--- client/components/MemberInfo.jsx
+++ client/components/MemberInfo.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "../styles/MemberInfo.module.css";
+<<<<<<< HEAD
 import { parseToEther } from "../util";
+=======
+import { formateEther } from "../util";
+>>>>>>> 0daaee3 (Updated balance events)
 
 const MemberInfo = ({ membersInfo }) => {
   return (
@@ -24,7 +28,11 @@ const MemberInfo = ({ membersInfo }) => {
               >
                 {x.address}:
               </a>{" "}
+<<<<<<< HEAD
               <span style={{}}>{parseToEther(x.balance)} ETH </span>
+=======
+              <span style={{}}>{formateEther(x.balance)} ETH </span>
+>>>>>>> 0daaee3 (Updated balance events)
             </div>
           );
         })}
